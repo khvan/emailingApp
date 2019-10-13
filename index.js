7fe25855e8d85c09c456c8d17d49963409e55a85
kBoris <khvanboris@gmail.com>
2019-10-13 14:37:34 -0700

    added models to the database, fixed creating records for the same users more than once

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
-const express = require('express');
-require('./services/passport');
+const express = require ('express');
+const mongoose = require ('mongoose');
+const keys = require ('./config/keys');
+require ('./models/User');
+require ('./services/passport');
 
+const app = express ();
 
+mongoose.connect (keys.mongoURI);
 
+require ('./routes/authRoutes') (app);
 
-
-const app = express();
-
-require('./routes/authRoutes')(app)
-
-app.get('/',(req,res) =>{
-  res.send({hello: "world?"});
+app.get ('/', (req, res) => {
+  res.send ({hello: 'world?'});
 });
 
-
-
-
-const PORT = process.env.PORT || 5000
-app.listen(PORT);
-
-
+const PORT = process.env.PORT || 5000;
+app.listen (PORT);
