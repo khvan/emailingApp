9b8a96c52a5f4ff1574746fddb3541449c88cd38
kBoris <khvanboris@gmail.com>
2019-10-14 23:11:17 -0700

    continued working on auth

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const keys = require ('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const
 require ('./models/User');
 require ('./services/passport');
 
--- services/passport.js
+++ services/passport.js
@@ -4,6 +4,17 @@ const mongoose = require ('mongoose');
 const keys = require ('../config/keys');
 const User = mongoose.model ('users');
 
+
+passport.serializeUser((user,done)=>{
+  done(null,user.id)
+});
+
+passport.deserializeUser((id,done)=>{
+  User.findById(id).then(user =>{
+    done(null,user);
+  })
+})
+
 passport.use (
   new GoogleStrategy (
     {
@@ -14,8 +25,11 @@ passport.use (
     (accessToken, refreshToken, profile, done) => {
       User.findOne ({googleId: profile.id}).then (existingUser => {
         if (existingUser) {
+          done(null, existingUser);
         } else {
-          new User ({googleId: profile.id}).save ();
+          new User ({googleId: profile.id})
+          .save()
+          .then(user => done(null, user))
         }
       });
     }
