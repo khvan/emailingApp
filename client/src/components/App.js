e133ad7b3132b9be7964e11856d279d78fb139b2
kBoris <khvanboris@gmail.com>
2019-10-25 13:20:31 -0700

    added a new homepage moved streamlist

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,7 @@ import StreamDelete from './streams/StreamDelete';
 import StreamEdit from './streams/StreamEdit';
 import StreamList from './streams/StreamList';
 import StreamShow from './streams/StreamShow';
-import HomePage from './'
+import HomePage from './HomePage';
 import history from '../history';
 import Header from './header';
 
